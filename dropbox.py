a7f49f1488ccdbea18b173f515a5af0063c8127c
amuhebwa <muhebwa.grey@gmail.com>
2017-01-17 06:01:16 +0200

    Added file with the model

---
+++ dropbox.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 21 12:26:16 2016
+
+@author: amuhebwa
+"""
+"""
+https://keras.io/#keras-deep-learning-library-for-theano-and-tensorflow
+
+http://machinelearningmastery.com/object-recognition-convolutional-neural-networks-keras-deep-learning-library/
+https://www.analyticsvidhya.com/blog/2016/10/tutorial-optimizing-neural-networks-using-keras-with-image-recognition-case-study/
+http://machinelearningmastery.com/regression-tutorial-keras-deep-learning-library-python/
+http://machinelearningmastery.com/tutorial-first-neural-network-python-keras/
+https://blog.keras.io/building-powerful-image-classification-models-using-very-little-data.html
+https://blog.keras.io/building-powerful-image-classification-models-using-very-little-data.html
+"""
+from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.layers import Activation,Dropout,Dense,Flatten
+from keras.constraints import maxnorm
+from keras.optimizers import SGD
+from keras.layers.convolutional import Convolution2D,MaxPooling2D
+from keras.utils import np_utils
+from keras import backend as K
+K.set_image_dim_ordering('th')
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train = x_train / 255.0
+X_test = x_test / 255.0
+
+
+# convert to one-hot encoder
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+
+number_of_classes = 10
+
+model = Sequential()
+model.add(Convolution2D(32, 3, 3, border_mode='same',input_shape=(3, 32, 32)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Convolution2D(32, 3, 3, border_mode='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Convolution2D(64, 3, 3, border_mode='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Convolution2D(128, 3, 3, border_mode='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+
+model.add(Dense(1024, activation='relu', W_constraint=maxnorm(3)))
+model.add(Dropout(0.2))
+model.add(Dense(number_of_classes, activation='softmax'))
+
+epochs = 50
+lrate = 0.01
+decay = lrate/epochs
+sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+print(model.summary())
+
+model.fit(x_train, y_train, validation_data=(X_test, y_test), nb_epoch=epochs, batch_size=128)
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
+
+
